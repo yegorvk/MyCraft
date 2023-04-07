b2cfa385616feada28c487daeb6581a4bebec086
egorv <egor.vaskonyan@gmail.com>
2023-04-07 19:14:03 +0200

    Update .gitignore

--- cmake-build-debug/_deps/glm-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/egorv/CLionProjects/ShitCraft/cmake-build-debug/_deps/glm-src
-# Build directory: C:/Users/egorv/CLionProjects/ShitCraft/cmake-build-debug/_deps/glm-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- cmake-build-debug/_deps/sdl2-build/include/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.26.5-gcf7d16e (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2.26.5-gcf7d16e"
-#endif
